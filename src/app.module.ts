be50a87c9aea940f68cb3d16353adece52e2c6ac
ashiryaev <ashiryaev@invitro.ru>
2023-07-04 00:51:06 +0300

    initialize

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ScheduleModule } from './schedule/schedule.module';
+import { RoomModule } from './room/room.module';
+
+@Module({
+  imports: [ScheduleModule, RoomModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
